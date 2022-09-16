83abe64e78b0a02d7c9e25ef6f66f83f8213e329
i.edgardo <95151027+Gato1502@users.noreply.github.com>
2022-09-16 17:07:36 -0500

    Update Proyecto Piero.ino

--- Proyecto Piero.ino
+++ Proyecto Piero.ino
@@ -1,4 +1,6 @@
-//=====================================Walid Mafuj===================================//
+//-------------------------Proyecto de feria de ciencias 2022-----------------------------------//
+
+//Importamos librerias del modulo RFID
 uint8_t buf[8] = { 0 };
 #include <SPI.h>
 #include <MFRC522.h> 
@@ -8,27 +10,28 @@ int gled = 7;
 int rled = 4;
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 String card_ID=""; // 
-String password="150231" ; // Change It To Your Windows / fb / any Account's Password
-String rfid="1604813527";// UID (unique Id Code Of Your Rfid Tag)
+String password="150231" ; // contraseña de nuestra computadora
+String rfid="1604813527"; // código la tarjeta RFID
+//declaramos nuestros pines de salida y entrada del modulo rfid
 void setup() {
-  Serial.begin(9600); // Initialize serial communications with the PC
-  SPI.begin();  // Init SPI bus
-  mfrc522.PCD_Init(); // Init MFRC522 card
+  Serial.begin(9600);
+  SPI.begin();  
+  mfrc522.PCD_Init(); 
   pinMode(gled,OUTPUT);
   pinMode(rled,OUTPUT);
 }
 void loop() {
-  //look for new card
+  
    if ( ! mfrc522.PICC_IsNewCardPresent()) {
   return;
  }
  if ( ! mfrc522.PICC_ReadCardSerial()) {
-  return;//if read card serial(0) returns 1, the uid struct contians the ID of the read card.
+  return;
  }
  for (byte i = 0; i < mfrc522.uid.size; i++) {
    card_ID += mfrc522.uid.uidByte[i];
  }
-// Serial.println(card_ID);
+
        if(card_ID==rfid){digitalWrite(gled,HIGH);
       typeLiteralString(password);
       pressKey("enter"); releaseKey("enter");
@@ -43,7 +46,7 @@ card_ID="";
 }
 boolean isModifier(int keycode) {
     boolean result = false;
-    if (keycode >= 224 && keycode <= 231) { // if the keycode is a modifier key
+    if (keycode >= 224 && keycode <= 231) { 
         result = true;
     }
     return result;
@@ -54,10 +57,10 @@ void pressModifier(String keyname) {
 }
 void pressModifier(int keycode) {
     int modifiermask = 0;
-    if (isModifier(keycode)) { // if the keycode represents a modifier key
+    if (isModifier(keycode)) { 
         modifiermask = getModifierMask(keycode);
         buf[0] = buf[0] | modifiermask;
-        Serial.write(buf, 8); // Send key report
+        Serial.write(buf, 8); 
     }
 }
 
@@ -66,48 +69,48 @@ void releaseModifier(String keyname) {
 }
 void releaseModifier(int keycode) {
     int modifiermask = 0;
-    if (isModifier(keycode)) { // if the keycode represents a modifier key
+    if (isModifier(keycode)) { 
         modifiermask = getModifierMask(keycode);
         buf[0] = buf[0] & (~modifiermask);
-        Serial.write(buf, 8); // Send key report
+        Serial.write(buf, 8); 
     }
 }
 void releaseAllModifiers() {
     buf[0] = B00000000;
-    Serial.write(buf, 8); // Send key report
+    Serial.write(buf, 8); 
 }
 
 void pressKey(String keyname) {
     pressKey(getKeycode(keyname));
 }
-void pressKey(int keycode) { // TODO: cycle the 6 key spots in the report buffer instead of just using buf[2] each time.
+void pressKey(int keycode) { 
     buf[2] = keycode;
-    Serial.write(buf, 8); // Send key report
+    Serial.write(buf, 8); 
 }
 
 void releaseKey(String keyname) {
     releaseKey(getKeycode(keyname));
 }
 void releaseKey(int keycode) {
-    // find the keycode in the report buffer, then set it to zero.
+  
     int i=0;
     for (i=2; i<8; i++) {
         if (buf[i] == keycode) {
             buf[i] = 0;
         }
     }
-    Serial.write(buf, 8); // Send key report
+    Serial.write(buf, 8);
 }
 void releaseAllKeys() {
     int i=0;
     for (i=2; i<8; i++) {
         buf[i] = 0;
     }
-    Serial.write(buf, 8); // Send key report
+    Serial.write(buf, 8);
 }
 
 void pressSequenceOfKeys(const char * keySequence[], int numberOfKeys) {
-    // This function can be good for pressing a few keys while holding a modifier down for example.
+   
 
     int i = 0;
     for (i=0; i<numberOfKeys; i++) {
@@ -121,8 +124,7 @@ void typeLiteralString(String string) {
     string.toCharArray(charArray, string.length()+1);
     typeLiteralString(charArray, string.length());
 }
-void typeLiteralString(char string[], int stringLength) { // stringLength is the length of the printable string without considering the null byte.
-    // This function will type the given string exactly as given, automatically pressing left_shift where necessary for capitals and symbols.
+void typeLiteralString(char string[], int stringLength) { 
 
     // just in case:
     releaseAllKeys();
@@ -142,8 +144,8 @@ void typeLiteralString(char string[], int stringLength) { // stringLength is the
             releaseModifier("left_shift");
             shiftIsPressed = false;
         }
-        pressKey(String(string[i])); // without converting the char in string[i] to a String, arduino would prefer the pressKey(int) function instead of the pressKey(String) function, casting the char to a keycode (int) instead of a keyname (String).
-        releaseKey(String(string[i])); // same as previous comment, but with releaseKey().
+        pressKey(String(string[i])); 
+        releaseKey(String(string[i])); 
     }
 
     releaseAllModifiers();
@@ -151,7 +153,7 @@ void typeLiteralString(char string[], int stringLength) { // stringLength is the
 
 boolean characterNeedsShift(char character) {
     int needsModifier = false;
-    if ( // look up an ascii table and this will make sense.
+    if ( 
         (character >= 33 && character <= 38)
         || (character >= 40 && character <= 43)
         || (character == 58)
@@ -166,12 +168,12 @@ boolean characterNeedsShift(char character) {
 }
 
 int getKeycode(String keyname) {
-    String key = String(keyname); // Use a copy so that we don't mutate the user's String. Not sure if this is needed, but just in case. TODO: find out.
+    String key = String(keyname); 
     key.toLowerCase();
 
-    int keycode = 0; // keycode of zero means nothing pressed.
+    int keycode = 0; 
 
-    // non-modifier keys
+    
     if      (key == "a") { keycode =  4; }
     else if (key == "b") { keycode =  5; }
     else if (key == "c") { keycode =  6; }
@@ -228,7 +230,7 @@ int getKeycode(String keyname) {
     else if (key == "." || key == ">")  { keycode = 55; }
     else if (key == "/" || key == "?")  { keycode = 56; }
 
-    // TODO: Fix these keycodes. V
+    
     else if (key == "capslock")         { keycode = 58; }
 
     else if (key == "f1")      { keycode = 59; }
@@ -261,9 +263,9 @@ int getKeycode(String keyname) {
 
     else if (key == "numlock" || key == "clear")   { keycode = 83; }
 
-    //TODO: keypad and miscellaneous keys if you want them.
+    
 
-    // modifier keys.
+   
     else if (key == "left_control")  { keycode = 224; }
     else if (key == "left_shift")    { keycode = 225; }
     else if (key == "left_alt")      { keycode = 226; }
@@ -280,10 +282,10 @@ int getKeycode(String keyname) {
 int getModifierMask(String keyname) {
     return getModifierMask(getKeycode(keyname));
 }
-int getModifierMask(int keycode) { // return value of 0 means key is not a modifier.
+int getModifierMask(int keycode) { 
     int modifiermask = 0;
 
-    // NOTE: these are not the usage keycodes like for other keys, but rather the bit masks.
+ 
     if      (keycode == 224) { modifiermask = B00000001; } // left ctrl
     else if (keycode == 225) { modifiermask = B00000010; } // left shift
     else if (keycode == 226) { modifiermask = B00000100; } // left alt
